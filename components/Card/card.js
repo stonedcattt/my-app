761566e45752e727e12947e824419fb854383d07
Rodrigo Moyano <rodrigomoyano11@gmail.com>
2022-08-10 18:37:35 -0300

    Se crearon los componentes "Card" y "Header"

---
+++ components/Card/card.js
@@ -0,0 +1,11 @@
+import styles from './card.module.css'
+
+const Card = () => {
+  return (
+    <div className={styles.box}>
+      <p>Card</p>
+    </div>
+  )
+}
+
+export default Card
---
+++ components/Header/header.js
@@ -0,0 +1,11 @@
+import styles from './header.module.css'
+
+const Header = () => {
+  return (
+    <div className={styles.box}>
+      <p>Header</p>
+    </div>
+  )
+}
+
+export default Header;
